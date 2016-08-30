9e7274499bc7273985957e3e486acba5587d9da4
Zaur Nasibov <zaur@znasibov.info>
2016-08-30 13:13:53 +0300

    Fix wfs.py

--- owslib/wfs.py
+++ owslib/wfs.py
@@ -19,7 +19,7 @@ from .feature import wfs100, wfs110, wfs200
 
 
 def WebFeatureService(url, version='1.0.0', xml=None, parse_remote_metadata=False,
-                      timeout=30, username=None, password=None):
+                      timeout=30, username=None, password=None, req_kwargs=None):
     ''' wfs factory function, returns a version specific WebFeatureService object
 
     @type url: string
@@ -31,20 +31,18 @@ def WebFeatureService(url, version='1.0.0', xml=None, parse_remote_metadata=Fals
     @param timeout: time (in seconds) after which requests should timeout
     @param username: service authentication username
     @param password: service authentication password
+    @param req_kwargs: a dictionary of additional HTTP requests parameters
     @return: initialized WebFeatureService_2_0_0 object
     '''
+    kwargs = {
+        'timeout': timeout,
+        'username': username,
+        'password': password,
+        'req_kwargs': req_kwargs,
+    }
     if version in  ['1.0', '1.0.0']:
-        return wfs100.WebFeatureService_1_0_0(url, version, xml, parse_remote_metadata,
-                                              timeout=timeout,
-                                              username=username,
-                                              password=password)
+        return wfs100.WebFeatureService_1_0_0(url, version, xml, parse_remote_metadata, **kwargs)
     elif version in  ['1.1', '1.1.0']:
-        return wfs110.WebFeatureService_1_1_0(url, version, xml, parse_remote_metadata,
-                                              timeout=timeout,
-                                              username=username,
-                                              password=password)
+        return wfs110.WebFeatureService_1_1_0(url, version, xml, parse_remote_metadata, **kwargs)
     elif version in ['2.0', '2.0.0']:
-        return wfs200.WebFeatureService_2_0_0(url,  version, xml, parse_remote_metadata,
-                                              timeout=timeout,
-                                              username=username,
-                                              password=password)
+        return wfs200.WebFeatureService_2_0_0(url,  version, xml, parse_remote_metadata, **kwargs)
